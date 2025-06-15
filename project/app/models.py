787cb237d51d46f9b0d27b00dd238c67d94d239e
carlatakagi <carlatakagi@gmail.com>
2025-06-15 11:30:35 -0400

    feat: add models

--- project/app/models.py
+++ project/app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class UserProfile(models.Model):
+    username = models.CharField('username',max_length=100)
+    email = models.EmailField('email',max_length=100)
+    phone= models.IntegerField('phone')
+
+    def __str__(self):
+        return f"Name: {self.username}, Email: {self.email}, Phone: {self.phone}"
